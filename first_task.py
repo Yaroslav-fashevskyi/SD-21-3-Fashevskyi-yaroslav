6d14d68bec498c80ab01f55bf5ef4888e232936b
pohorelova-julia <yuliia.i.pohorelova@ukd.edu.ua>
2025-02-12 10:59:03 +0200

    Add my line

--- first_task.py
+++ first_task.py
@@ -3,3 +3,5 @@ print("Hi, my name is Фашевський Ярослав Ігорович and I
 print("Hi, my name is Дячок Олег Романович and I’m studying software development in college")
 
 print("Hi, my name is Іванілова Аліна Олексіївна and I’m studying software development in college")
+
+print("Hi, my name is Погорелова Юлія Ігорівна and I’m studying software development in college")
